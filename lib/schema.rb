207ed8f3f0c0d8a9201002a07613f4ce70382570
Jim Rodovich <jim@inventables.com>
2013-09-04 08:30:12 -0500

    Add method to print migration plan consistently

--- lib/schema.rb
+++ lib/schema.rb
@@ -1,6 +1,16 @@
 module Schema
   MIGRATIONS_PATH = 'db/migrate/'
 
+  def Schema.print_plan(files)
+    rollbacks = files[:rollbacks] || []
+    migrations = files[:migrations] || []
+    plan = []
+    plan << "#{rollbacks.count} down" if rollbacks.any?
+    plan << "#{migrations.count} up" if migrations.any?
+    puts "Migrating:  #{plan.join(', ')}" if plan.any?
+    puts "0 migrations" if plan.empty?
+  end
+
   def Schema.migrate(files)
     system "rake db:migrate" unless files.empty?
   end
